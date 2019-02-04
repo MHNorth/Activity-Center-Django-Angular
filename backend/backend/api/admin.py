c6ffb174e45636ada5f5f2d6426bfbe2110c69ed
Maika Northrop <maikanorthrop@gmail.com>
2019-02-03 20:05:36 -0500

    dev backend authorization

--- backend/backend/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import ActivityCenter
-
-
-admin.site.register(ActivityCenter)
-
---
+++ django_activity/api_app/serializers.py
@@ -0,0 +1,16 @@
+# from django.contrib.auth.models import AbstractUser
+# from .models import ActivityCenter
+# from rest_framework import serializers
+
+
+# class UserSerializer(serializers.ModelSerializer):
+
+#         class Meta:
+#                 model = User
+#                 fields = ('id', 'email')
+
+# class ActivityCenterSerializer(serializers.ModelSerializer):
+
+#         class Meta:
+#                 model = ActivityCenter
+#                 fields = ('id', 'activity', 'coordinator', 'duration', 'event date', 'event time')
